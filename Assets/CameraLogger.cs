ae71aacab826d837e3b59557ddaf18b615ab98c1
PrivateVoid <raphael.royer.rivard@gmail.com>
2016-05-14 02:47:39 -0400

    block color changes

---
+++ Assets/CameraLogger.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class CameraLogger : MonoBehaviour, HeadMovementListener {
+    GameObject cube;
+    HeadMovementDetector detector;
+
+    // Use this for initialization
+    void Start ()
+    {
+        cube = GameObject.Find("Cube");
+        detector = new HeadMovementDetector(this);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        detector.Update();
+    }
+
+    public void HeadMoved()
+    {
+        cube.GetComponent<Renderer>().material.color = Color.red;
+    }
+
+    public void HeadStopped(float elapsedTime)
+    {
+        cube.GetComponent<Renderer>().material.color = Color.green;
+        Debug.Log("Head stopped after " + elapsedTime + " seconds of movement");
+    }
+}
