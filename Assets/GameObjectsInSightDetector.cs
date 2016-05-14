f0146b305d9d68f89e1b4d51206ef63f4fd6fb14
PrivateVoid <raphael.royer.rivard@gmail.com>
2016-05-14 08:16:36 -0400

    in and out of sight detection

---
+++ Assets/GameObjectsInSightDetector.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// This script must be attached to a GameObject that has a render.
+/// </summary>
+public class GameObjectsInSightDetector : MonoBehaviour {
+    private bool toggle = false;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    void OnBecameInvisible()
+    {
+        GetComponent<Renderer>().material.color = toggle ? Color.black : Color.white;   //TODO do something else
+        toggle = !toggle;
+        Debug.Log(this.name + " is invisible and " + (toggle ? "" : "not") + " toggled");
+    }
+
+    void OnBecameVisible()
+    {
+        Debug.Log(this.name + " is visible");
+    }
+}
