33d2fb9e523eb4b105e98eba27928a78e19ca403
PrivateVoid <raphael.royer.rivard@gmail.com>
2016-05-14 03:53:17 -0400

    action detection (joystick A button, spacebar and left click

---
+++ Assets/PlayerActionDetector.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+/// <summary>
+/// This class will check for Controller's base button press, spacebar key press and left click.
+/// </summary>
+public class PlayerActionDetector
+{
+    private PlayerActionListener listener;
+    private bool pressing = false;
+
+    public PlayerActionDetector(PlayerActionListener listener)
+    {
+        this.listener = listener;
+    }
+
+    public void Update()
+    {
+        if (Input.GetKey(KeyCode.Joystick1Button0) || Input.GetKeyDown("space") || Input.GetMouseButtonDown(0)) {
+            if(!pressing)
+            {
+                listener.ActionDetected();
+                pressing = true;
+            }
+        }
+        else
+        {
+            pressing = false;
+        }
+    }
+}
